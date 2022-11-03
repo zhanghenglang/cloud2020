a9ff27a97208ae5f5327113dacfcc668c0e9e583
zhanghenglang <576919088@qq.com>
2022-11-03 15:44:02 +0800

    springcloud:config动态刷新

--- cloud-config-client-3366/src/main/java/com/demo/springcloud/ConfigClientMain3366.java
+++ cloud-config-client-3366/src/main/java/com/demo/springcloud/ConfigClientMain3366.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class ConfigClientMain3355 {
+public class ConfigClientMain3366 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigClientMain3355.class,args);
+        SpringApplication.run(ConfigClientMain3366.class,args);
     }
 }
